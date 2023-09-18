d2f2ed9e9aa931d750e1b27592b1dd94e748fb80
MD Rony Mia <mdronymia.javascript@gmail.com>
2023-09-18 22:57:02 +0600

    project init

---
+++ src/Layouts/MainLayout.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function MainLayout() {
+     return (
+          <div>
+               <h1>
+                    hello world
+               </h1>
+          </div>
+     )
+}
+
+export default MainLayout;
