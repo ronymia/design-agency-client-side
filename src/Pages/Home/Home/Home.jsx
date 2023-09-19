d32a65c0a018d9fbad67fc58fe1371629f5ec8d3
MD Rony Mia <mdronymia.javascript@gmail.com>
2023-09-19 09:16:27 +0600

    axios, react query & service section done

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Banner from '../Banner/Banner';
 import Client from '../Client/Client';
+import Services from '../Services/Services';
 
 export default function Home() {
      return (
           <div>
                <Banner />
                <Client />
+               <Services />
           </div>
      )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './Routes/routes/Routes.jsx';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
+
+//Create a client
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   </React.StrictMode>,
 )
