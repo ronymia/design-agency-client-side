db7e4da411b0d1a3855cb3bace32bcbdf52a4f38
MD Rony Mia <mdronymia.javascript@gmail.com>
2023-09-20 23:20:23 +0600

    contact left

--- src/Pages/Home/Contact/Contact.jsx
+++ src/Pages/Home/Contact/Contact.jsx
@@ -2,8 +2,19 @@ import React from 'react';
 
 export default function Contact() {
      return (
-          <div>
-
-          </div>
+          <section className='w-full h-[400px] bg-yellow px-24 py-10 flex items-start justify-center gap-16'>
+               <div className="">
+                    <h2 className='text-3xl leading-normal font-semibold mb-8'>
+                         Let us handle your <br /> project, professionally.
+                    </h2>
+                    <p className='font-light text-sm'>
+                         With well written codes, we build amazing apps for all <br /> platforms, mobile and web apps in general.
+                    </p>
+               </div>
+               <div className="">
+                    <input type="text" name="" id="" />
+                    <input type="text" name="" id="" />
+               </div>
+          </section>
      )
 }
