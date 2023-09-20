937e7cea98f2e27b5b7cf6d5be31736c5f4e1953
MD Rony Mia <mdronymia.javascript@gmail.com>
2023-09-21 00:13:50 +0600

    contact form

--- src/Pages/Home/Contact/Contact.jsx
+++ src/Pages/Home/Contact/Contact.jsx
@@ -11,27 +11,35 @@ export default function Contact() {
                          With well written codes, we build amazing apps for all <br /> platforms, mobile and web apps in general.
                     </p>
                </div>
-               <div className="flex flex-col gap-y-4">
-                    <input
-                         className='w-96 h-11 rounded focus:outline-none text-sm px-4 py-3 placeholder:text-xs placeholder:font-light placeholder:capitalize'
-                         type="text"
-                         name=""
-                         id=""
-                         placeholder='your email address'
-                    />
-                    <input
-                         className='w-96 h-11 rounded focus:outline-none text-sm px-4 py-3 placeholder:text-xs placeholder:font-light placeholder:capitalize'
-                         type="text"
-                         name=""
-                         id=""
-                         placeholder="your name / company's name"
-                    />
-                    <textarea
-                         className='resize-none rounded w-96 h-40 focus:outline-none text-sm px-5 py-4 placeholder:text-sm placeholder:font-light placeholder:capitalize'
-                         name=""
-                         id=""
-                         placeholder='your massage'
-                    ></textarea>
+               <div className="">
+                    <form action=""
+                         className='flex flex-col gap-y-4'
+                    >
+                         <input
+                              className='w-96 h-11 rounded focus:outline-none text-sm px-4 py-3 placeholder:text-xs placeholder:font-light placeholder:capitalize'
+                              type="text"
+                              name=""
+                              id=""
+                              placeholder='your email address'
+                         />
+                         <input
+                              className='w-96 h-11 rounded focus:outline-none text-sm px-4 py-3 placeholder:text-xs placeholder:font-light placeholder:capitalize'
+                              type="text"
+                              name=""
+                              id=""
+                              placeholder="your name / company's name"
+                         />
+                         <textarea
+                              className='resize-none rounded w-96 h-40 focus:outline-none text-sm px-5 py-4 placeholder:text-sm placeholder:font-light placeholder:capitalize'
+                              name=""
+                              id=""
+                              placeholder='your massage'
+                         ></textarea>
+                         <input type="button" value="send"
+                              className="px-[35px] py-[10px] bg-primary text-white rounded-[5px] 
+                              cursor-pointer uppercase tracking-widest"
+                         />
+                    </form>
                </div>
           </section>
      )
