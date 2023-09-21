11eae7c72d01a05a17bbb7978d6463ebfd45d019
MD Rony Mia <mdronymia.javascript@gmail.com>
2023-09-22 00:18:45 +0600

    user Auth

--- src/Routes/routes/Routes.jsx
+++ src/Routes/routes/Routes.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from "react-router-dom";
 import MainLayout from "../../Layouts/MainLayout";
 import Home from "../../Pages/Home/Home/Home";
+import { Login } from "../../Pages/UserAuth";
 
 export const router = createBrowserRouter([
      {
@@ -12,5 +13,9 @@ export const router = createBrowserRouter([
                     element: <Home />
                }
           ]
+     },
+     {
+          path: "/login",
+          element: <Login />
      }
 ]);
